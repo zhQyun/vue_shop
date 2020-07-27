5c069747ca43e9297047d9f3f344cbf04590eb9c
zhQyun <13477816608@163.com>
2020-07-28 07:49:07 +0800

    新增

--- src/pages/custom/MenuSettings.vue
+++ src/pages/custom/MenuSettings.vue
@@ -4,29 +4,44 @@
 
   <el-row>
   <el-button>添加一级菜单</el-button>
-  
+ <el-form :inline="true" :model="formInline" class="demo-form-inline">
+  <el-form-item label="一级菜单名">
+    <el-input v-model="formInline.user" ></el-input>
+  </el-form-item>
+   <el-form-item>
+    <el-button type="primary" @click="onSubmit">删除</el-button>
+  </el-form-item>
+  <el-form-item label="链接">
+    <el-select v-model="formInline.region" >
+     <el-input></el-input>
+    </el-select>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">删除</el-button>
+  </el-form-item>
+   <el-button>添加二级菜单</el-button>
+</el-form>
 </el-row>
 </div>
 
 </template>
 
 <script>
-export default {
-    name:"",
+ export default {
     data() {
-        return {
-
+      return {
+        formInline: {
+          user: '',
+          region: ''
         }
+      }
     },
-    //生命周期 - 创建完成（访问当前this实例）
-    created() {
-
-    },
-    //生命周期 - 挂载完成（访问DOM元素）
-    mounted() {
-
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
     }
-}
+  }
 </script>
 <style scoped>
 *{text-align: initial;
