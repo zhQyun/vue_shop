aa4e85d3674ce56d7fb2b7756c7c0b30ae3c6204
zhQyun <13477816608@163.com>
2020-07-29 15:58:53 +0800

    新增自定义1

--- src/pages/custom/custom1.vue
+++ src/pages/custom/custom1.vue
@@ -2,49 +2,65 @@
 <template>
 <div>
   <div class="table">
-    <el-row v-for="(item,index) in table" :key='index'>
-
-  <el-button>{{item.name}}</el-button> 
-   
-          <el-button >删除</el-button>
-       
-  
-</el-row>
- <el-input v-model="inputValue" style="width:75px;"  class="input" ></el-input> <el-button @click="add()">添加</el-button>
+<el-tag
+  v-for="tag in tags"
+  :key="tag"
+  closable
+  @close="DeleteName(tag)"
+>
+  {{tag}}
+</el-tag>
+<el-button class="button" v-if="!open" style="padding:1px 5px" @click="OpenInput()">+new tag</el-button>
+<el-input class="input" v-model="inputname" v-if="open" @keyup.enter.native="submit"></el-input>
   </div>
 </div>
 </template>
 
 <script>
 export default {
-    name:"",
-    data() {
-        return {
-          inputValue: '11',
-          table: [
-          { name: '标签一' },
-          { name: '标签二' },
-          { name: '标签三' },
-          { name: '标签四' },
-          { name: '标签五' }
- 
-          ]
-
-        }
+ data() {
+      return {
+        open:false,
+        inputname:'',
+        tags: ['标签一','标签二','标签三','标签四']
+      };
     },
-    //生命周期 - 创建完成（访问当前this实例）
-    created() {
-
-    },
-    //生命周期 - 挂载完成（访问DOM元素）
-    mounted() {
-      add() {
-
+    methods:{
+      OpenInput() {
+       
+        this.open=true;
+      },
+      submit() {
+         console.log(this.tags);
+        this.open=false;
+        this.tags.push(this.inputname);
+        this.inputname='';
+        
+      },
+      DeleteName(tag) {
+       
+         this.tags.splice(this.tags.indexOf(tag), 1);
+      
       }
+     
     }
 }
 </script>
 <style scoped >
+.button {
+    height: 24px;
+    width: 73.34px;
+    line-height: 20px;
+    padding-top: 0;
+    padding-bottom: 0;
+    vertical-align: bottom;
 
+}
+.input {
+  height: 24px;
+    width: 73.34px;
+    margin-left: 10px;
+    vertical-align: bottom;
+}
 
 </style>
